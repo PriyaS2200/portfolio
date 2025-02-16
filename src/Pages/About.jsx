8a0e647ecb5256da9d8eb2b5899c99b90f501e1b
PriyaS2200 <sonkarpriya667@gmail.com>
2025-02-16 19:16:34 +0530

    Done with Home page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,44 @@
-import { Text } from "@chakra-ui/react"
+import { Box, Flex, Image, Text, Heading } from "@chakra-ui/react"
 
 export const About = () => {
     return (
-        <Text>This is about</Text>
+        <Box
+            p={5}
+        >
+            <Heading
+                color={"blackAlpha.600"}
+                fontWeight={"bolder"}
+                fontFamily={"sans-serif"}
+                fontSize={"3xl"}
+                textShadow={"2px 2px 5px wheat"}
+                textAlign={"center"}
+                p={5}
+            >
+                About Me!
+            </Heading>
+            <Flex
+                justifyContent={"space-around"}
+                alignItems={"center"}
+                p={5}
+                mt={10}
+            >
+                <Image
+                    boxShadow={"2px 5px 10px wheat"}
+                    borderRadius={10}
+                    border={"none"}
+                    src="portfolio\src\assets\passportPhoto.png" />
+                <Text
+                    textAlign={"center"}
+                >   I’m a Computer Science Engineering graduate with a strong foundation
+                    in programming and problem-solving. I’m currently learning frontend development,
+                    where I’m focusing on tools and frameworks like HTML, CSS, JavaScript, and React.
+                    During my time at university, I worked on several software development projects,
+                    and in my final year, I developed a small website, which ignited my interest in frontend
+                    technologies. I’m passionate about creating clean, responsive, and user-friendly web interfaces.
+                    My goal is to continue improving my skills, build a strong portfolio,
+                    and eventually contribute to building innovative digital products as a frontend developer.
+                </Text>
+            </Flex>
+        </Box>
     )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,50 @@
-import { Text } from "@chakra-ui/react"
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react"
 import { About } from "./About"
 import { Skills } from "./Skills"
 import { Project } from "./Project"
 import { Contact } from "./Contact"
 
 export const Home = () => {
-    return (
-        <>
-          <Text>This is home</Text>
-          <About />
-          <Skills />
-          <Project />
-          <Contact />
-        </>
-      
-    )
+  return (
+    <>
+      <Box
+      p={5}
+      h={550}
+      >
+        <Flex 
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        gap={4}
+        mt={4}
+        >
+          <Heading
+          color={"blackAlpha.600"}
+          fontWeight={"bolder"}
+          fontFamily={"sans-serif"}
+          fontSize={"3xl"}
+          textShadow={"2px 2px 5px wheat"}
+          >Hello! 🤝</Heading>
+          <Text
+          color={"gray.600"}
+          fontFamily={"-moz-initial"}
+          fontWeight={"bold"}
+          fontSize={"3xl"}
+          textShadow={"2px 2px 5px wheat"}
+          p={4}
+          >I am Priya Sonkar 👩🏻‍💻</Text>
+          <Image
+          boxShadow={"2px 5px 10px wheat"}
+          borderRadius={10}
+          border={"none"}
+          src="portfolio\src\assets\home.png" /> 
+        </Flex>
+      </Box>
+      <About />
+      <Skills />
+      <Project />
+      <Contact />
+    </>
+
+  )
 }
